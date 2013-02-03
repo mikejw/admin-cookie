f3ea6abfd5d3165c7b707d0e6d8cfd5418c985d7
Mike Whiting <mail@mikejw.co.uk>
2013-02-03 21:32:18 +0000

    initial import.

---
+++ src/Empathy/AdminCookie.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace RtnAdmin;
+
+function set_returning_admin_user()
+{
+    $now = new \DateTime();
+    $now->add(new \DateInterval('P2Y'));
+    $stamp = $now->getTimestamp();
+    if(!is_returning_admin_user()) {
+        setcookie('returning_admin_user', $stamp, $stamp);
+    }
+}
+
+function is_returning_admin_user()
+{
+    return isset($_COOKIE['returning_admin_user'])? true: false;
+}
+
+function get_returning_admin_expire()
+{
+    return date('d/m/Y H:i:s', $_COOKIE['returning_admin_user']);
+}
+
