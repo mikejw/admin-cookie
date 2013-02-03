0757058e44b18636b586b65e0c35f1e59a4eb957
Mike Whiting <mail@mikejw.co.uk>
2013-02-03 22:51:42 +0000

    now using a class with static methods.

--- src/Empathy/AdminCookie.php
+++ src/Empathy/AdminCookie.php
@@ -1,24 +1,27 @@
 <?php
 
-namespace Empathy\AdminCookie;
+namespace Empathy;
 
-function set_returning_admin_user()
+class AdminCookie
 {
-    $now = new \DateTime();
-    $now->add(new \DateInterval('P2Y'));
-    $stamp = $now->getTimestamp();
-    if(!is_returning_admin_user()) {
-        setcookie('returning_admin_user', $stamp, $stamp);
+
+    public static function set_returning_admin_user()
+    {
+        $now = new \DateTime();
+        $now->add(new \DateInterval('P2Y'));
+        $stamp = $now->getTimestamp();
+        if(!is_returning_admin_user()) {
+            setcookie('returning_admin_user', $stamp, $stamp);
+        }
+    }
+
+    public static function is_returning_admin_user()
+    {
+        return isset($_COOKIE['returning_admin_user'])? true: false;
+    }
+
+    function get_returning_admin_expire()
+    {
+        return date('d/m/Y H:i:s', $_COOKIE['returning_admin_user']);
     }
 }
-
-function is_returning_admin_user()
-{
-    return isset($_COOKIE['returning_admin_user'])? true: false;
-}
-
-function get_returning_admin_expire()
-{
-    return date('d/m/Y H:i:s', $_COOKIE['returning_admin_user']);
-}
-
