a2c98454dca3a076288fff60f6cfed72200d5a86
Mike Whiting <mail@mikejw.co.uk>
2013-02-04 12:04:51 +0000

    setting cookie in root of path.

--- src/Empathy/AdminCookie.php
+++ src/Empathy/AdminCookie.php
@@ -5,13 +5,23 @@ namespace Empathy;
 class AdminCookie
 {
 
-    public static function set_returning_admin_user()
+    public static function set_returning_admin_user($path)
     {
+        if(strpos($path, 'http://') === 0) {
+            $path = str_replace('http://', '', $path);
+        }
+        // remove host
+        $path_arr = explode('/', $path);
+        array_shift($path_arr);
+        $path = '/'.implode('/', $path_arr);
+        if(!preg_match('/\/$/', $path)) {
+            $path .= '/';
+        }       
         $now = new \DateTime();
         $now->add(new \DateInterval('P2Y'));
         $stamp = $now->getTimestamp();
-        if(!is_returning_admin_user()) {
-            setcookie('returning_admin_user', $stamp, $stamp);
+        if(!self::is_returning_admin_user()) {           
+            setcookie('returning_admin_user', $stamp, $stamp, $path);
         }
     }
 
@@ -20,7 +30,7 @@ class AdminCookie
         return isset($_COOKIE['returning_admin_user'])? true: false;
     }
 
-    function get_returning_admin_expire()
+    public static function get_returning_admin_expire()
     {
         return date('d/m/Y H:i:s', $_COOKIE['returning_admin_user']);
     }
